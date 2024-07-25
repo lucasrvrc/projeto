4c67991bafda4602e6012210943db67c72fdf918
paizim <133986381+paizim@users.noreply.github.com>
2024-07-25 11:34:26 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// verificar_cep.h
+
+#ifndef VERIFICAR_CEP_H
+#define VERIFICAR_CEP_H
+
+// Declaração da função verificarCEP
+int verificarCEP(const char* cep);
+
+#endif // VERIFICAR_CEP_H
+
+//verificar cpf.h
+#ifndef VERIFICAR_CPF_H
+#define VERIFICAR_CPF_H
+
+int verificarCPF(const char* cpf);
+
+#endif // VERIFICAR_CPF_H
+
+//verificar campos em branco.h
+#ifndef VALIDACAO_H
+#define VALIDACAO_H
+
+// Protótipo da função para validar campo
+int validarCampo(char campo[]);
+
+#endif /* VALIDACAO_H */
+
+
+
+
+
+
