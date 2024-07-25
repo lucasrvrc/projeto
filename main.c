3cbc95d28dfaf8dfd4e2f95936d8acbe1ba28ff7
paizim <133986381+paizim@users.noreply.github.com>
2024-07-25 11:50:59 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+
+#ifndef VERIFCADOR_H
+#define VERIFICADOR_H
+
+
+int verificarCEP(const char* cep);
+
+int verificarCPF(const char* cpf);
+
+int validarCampo(char campo[]);
+
+#endif
+
+
+
+
+
+
