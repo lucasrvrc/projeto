7f651b6c28831ae9e2b20ed3b232c22a744e6634
lucasrvrc <lucasrvrc@gmail.com>
2024-07-26 13:23:20 -0300

    Delete verificador.h

--- verificador.h
+++
@@ -1,9 +0,0 @@
-#ifndef VERIFICADOR_H
-#define VERIFICADOR_H
-
-int verificarCEP(const char* cep);
-int verificarCPF(const char* cpf);
-int validarCampo(char campo[]);
-int TratamentoLinha(char *linha, GtkTextBuffer *text_buffer);
-
-#endif
