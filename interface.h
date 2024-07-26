bf304186bafe8ea118f6706d73f5fe9523b12641
lucasrvrc <lucasrvrc@gmail.com>
2024-07-26 13:22:50 -0300

    Delete interface.h

--- interface.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTERFACE_H
-#define INTERFACE_H
-
-#include <gtk/gtk.h>
-
-void inicializar_interface(int argc, char *argv[]);
-void validar_planilha(const gchar *filename, GtkWidget *resultado_text_view);
-
-#endif
